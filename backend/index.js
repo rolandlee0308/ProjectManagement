ed71e08d30183c63d7760fc786c8c70c37ea7371
Roland Lee <rolandlee500@gmail.com>
2022-11-03 22:34:02 -0700

    connect backend to mongodb

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,11 @@ const express = require("express");
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 const { graphqlHTTP } = require("express-graphql");
+const connectDB = require("./config/db");
 const schema = require("./schema/schema");
 
 const app = express();
+connectDB();
 
 app.use(
   "/graphql",
--- backend/schema/schema.js
+++ backend/schema/schema.js
@@ -1,4 +1,5 @@
-const { projects, clients } = require("../_mockData");
+const Project = require("../models/Project");
+const Client = require("../models/Client");
 const {
   GraphQLObjectType,
   GraphQLID,
@@ -17,20 +18,49 @@ const ClientType = new GraphQLObjectType({
   }),
 });
 
+const ProjectType = new GraphQLObjectType({
+  name: "Project",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+    client: {
+      type: ClientType,
+      resolve(parent, args) {
+        return Client.findById(parent.clientId);
+      },
+    },
+  }),
+});
+
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
     clients: {
       type: new GraphQLList(ClientType),
       resolve(parent, args) {
-        return clients;
+        return Client.find();
       },
     },
     client: {
       type: ClientType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return clients.find((client) => client.id === args.id);
+        return Client.findById(args.id);
+      },
+    },
+    projects: {
+      type: new GraphQLList(ProjectType),
+      resolve(parent, args) {
+        return Project.find();
+      },
+    },
+    project: {
+      type: ProjectType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return Project.findById(args.id);
       },
     },
   },
